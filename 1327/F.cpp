3613c2e13012d26e33952aefdd5ed114d5a931bc
Kilo-5723 <yunxiang.zhao.stu@gmail.com>
2023-01-02 00:01:19 +0800

    upd

--- 1327/F.cpp
+++ 1327/F.cpp
@@ -1,15 +1,46 @@
-<<<<<<< HEAD
 #include <cstdio>
 #include <iostream>
+#include <vector>
 using namespace std;
-void solve(int n,vector<int>)
+typedef long long ll;
+const ll mod = 998244353;
+struct range {
+  int l, r, v;
+};
+ll solve(int n, const vector<range>& arr) {
+  vector<int> a(n + 2);
+  vector<int> b(n + 1, 0);
+  for (auto [l, r, v] : arr)
+    if (v) {
+      a[l]++;
+      a[r + 1]--;
+    } else {
+      b[r] = max(b[r], l);
+    }
+  for (int i = 1; i <= n; i++) {
+    a[i] += a[i - 1];
+    b[i] = max(b[i], b[i - 1]);
+  }
+  vector<ll> dp(n + 1);
+  dp[0] = 1;
+  ll sum = 0;
+  for (int r = 1, l = 0; r <= n; r++)
+    if (!a[r]) {
+      while (l < b[r]) {
+      }
+    }
+}
 int main() {
   int n, k, m;
   scanf("%d%d%d", &n, &k, &m);
+  vector a(k, vector<range>(m));
+  for (int i = 0; i < m; i++) {
+    int l, r, v;
+    scanf("%d%d%d", &l, &r, &v);
+    for (int j = 0; j < k; j++) a[j][i] = {l, r, v >> j & 1};
+  }
+  ll ans = 1;
+  for (const auto& arr : a) ans = ans * solve(n, arr) % mod;
+  printf("%lld\n", ans);
   return 0;
 }
-=======
-#include<iostream>
-#include<cstdio>
-using namespace std;
->>>>>>> b7a35a042894eb400e810f2536fb9f29752df11b
--- 1766/F.cpp
+++ 1766/F.cpp
@@ -5,18 +5,57 @@
 #include "../template/graph/costflow.h"
 
 using namespace std;
+using namespace costflow;
 int main() {
   int n, m;
   scanf("%d%d", &n, &m);
-  vector<costflow::flow_t> excess(n + 1, 0);
-  vector<costflow::CostEdge> edges;
-  while (m--) {
-    int u, v, c, w;
-    scanf("%d%d%d%d", &u, &v, &c, &w);
-    excess[u] -= c & 1;
-    excess[v] += c & 1;
-    edges.push_back({.from = u, .to = v, .cost = w * 2, .cap = c / 2});
+  vector<flow_t> excess(n + 1);
+  flow_t flow = 0;
+  cost_t totc = 0;
+  vector<CostEdge> edge;
+  vector<int> flg, flg2;
+  for (int i = 0; i < m; i++) {
+    int x, y, c, w;
+    scanf("%d%d%d%d", &x, &y, &c, &w);
+    flg.push_back(c & 1);
+    if (c & 1) {
+      excess[x]--;
+      excess[y]++;
+      totc += w;
+    }
+    w *= 2;
+    c /= 2;
+    edge.push_back({.from = x, .to = y, .cost = w, .cap = c});
   }
-  costflow::CostFlow cf;
+  if (excess[1] & 1) {
+    excess[1]--;
+    excess[n]++;
+  }
+  for (auto &v : excess) {
+    if (v & 1) {
+      puts("Impossible");
+      return 0;
+    }
+    v /= 2;
+  }
+  for (int i = 1; i <= n; i++) {
+    if (excess[i] > 0)
+      edge.push_back({.from = n + 1, .to = i, .cost = 0, .cap = excess[i]});
+    if (excess[i] < 0)
+      edge.push_back({.from = i, .to = n + 2, .cost = 0, .cap = -excess[i]});
+  }
+  edge.push_back({.from = n, .to = 1, .cost = 0, .cap = inf_flow});
+  maximum_flow(edge, n + 1, n + 2);
+  edge.pop_back();
+  for (int i = m; i < edge.size(); i++)
+    if (edge[i].flow != edge[i].cap) {
+      puts("Impossible");
+      return 0;
+    }
+  edge.resize(m);
+  puts("Possible");
+  edge.pop_back();
+  for (int i = 0; i < m; i++) printf("%lld ", edge[i].flow * 2 + flg[i]);
+  puts("");
   return 0;
 }
